ac8a55c3cff85a7f7b7cf6ab9016eb44be6bd54a
MiaKoring <143010553+MiaKoring@users.noreply.github.com>
2024-12-12 18:19:17 +0100

    day11 initial

---
+++ Day11/input.swift
@@ -0,0 +1,7 @@
+//
+//  input.swift
+//  AOC2024
+//
+//  Created by Mia Koring on 12.12.24.
+//
+
---
+++ Day11/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Day11
+//
+//  Created by Mia Koring on 12.12.24.
+//
+
+import Foundation
+
+print("Hello, World!")
+
