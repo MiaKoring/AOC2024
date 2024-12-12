8114c87bc45a19cb823c5a59919276cfe669d2a3
MiaKoring <143010553+MiaKoring@users.noreply.github.com>
2024-12-12 19:08:52 +0100

    Day11 solved

--- Day11/main.swift
+++ Day11/main.swift
@@ -7,5 +7,45 @@
 
 import Foundation
 
-print("Hello, World!")
+let sample = """
+125 17
+"""
 
+func parse(_ input: String) -> [Int] {
+    return input.split(separator: " ").compactMap { Int($0) }
+}
+
+func main(_ input: String) {
+    let parsed = parse(input)
+    
+    var stones = [Int: Int]()
+    for item in parsed {
+        stones[item] = 1
+    }
+    
+    for _ in 0..<75 {
+        var new = [Int: Int]()
+        for key in stones.keys {
+            if key == 0 {
+                new[1] = (new[1] ?? 0) + (stones[0] ?? 0)
+            } else if "\(key)".count % 2 == 0 {
+                guard let amount = stones[key] else { continue }
+                let str = "\(key)"
+                if let left = Int(str.prefix(str.count / 2)), let right = Int(str.suffix(str.count / 2)) {
+                    new[left] = (new[left] ?? 0) + amount
+                    new[right] = (new[right] ?? 0) + amount
+                }
+            } else {
+                new[key * 2024] = (new[key * 2024] ?? 0) + (stones[key] ?? 0)
+            }
+        }
+        stones = new
+    }
+    
+    print(stones.values.reduce(0, {$0 + $1}))
+}
+
+let time = DispatchTime.now().uptimeNanoseconds
+main(input)
+let end = Double(DispatchTime.now().uptimeNanoseconds - time) / 1000000000.0
+print(end)
